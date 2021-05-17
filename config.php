46556f65ba873c962e3c5f285749061e97823164
Okorie Chinedu <okoriechinedusunday@gmail.com>
2021-05-17 17:43:25 +0100

    fix some bug

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'us-cdbr-east-03.cleardb.com');
-define('DB_USERNAME', 'ba02a532ed0ccc');
+define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'heroku_875d3f0a300d0e0');
 define('DB_NAME', 'smarttrackerdb');
  
